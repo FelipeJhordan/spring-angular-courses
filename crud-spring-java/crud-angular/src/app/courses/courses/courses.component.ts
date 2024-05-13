bb4cfdf3ce1d79bffefc7ff8fa88f8b70eed3081
felipejhordan <felipejordan.alves@gmail.com>
2024-05-13 14:23:51 -0300

    feat(front-end):    create the table to show courses

--- crud-spring-java/crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-spring-java/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
-
+   public courses: Course[] = [
+    {
+      _id: '1',
+      name: 'Angular',
+      category: 'front-end'
+    }
+   ]
+   displayedColumns = [ 'name', 'category' ]
 }
