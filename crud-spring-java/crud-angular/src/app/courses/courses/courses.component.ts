67815caf5d48a0f6872e59ea803a3d16d5b54190
felipejhordan <felipejordan.alves@gmail.com>
2024-05-14 10:52:52 -0300

    feat(front-end):   finish list screen

--- crud-spring-java/crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-spring-java/crud-angular/src/app/courses/courses/courses.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { MatTableModule } from '@angular/material/table';
 import { CoursesService } from '../services/courses.service';
-import { Observable, delay, first, tap } from 'rxjs';
+import { Observable, catchError, delay, first, of, tap } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -15,14 +17,23 @@ export class CoursesComponent {
   displayedColumns = [ 'name', 'category' ]
 
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
     this.courses$ = this.coursesService.list().pipe(
-      first(),
-      delay(5000),
-      tap(courses => console.log(courses))
+     catchError( error => {
+      this.onError('Erro ao carregar cursos.')
+      return of([])
+     })
     )
   }
 
 
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    })
+  }
 
 }
--- crud-spring-java/crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-spring-java/crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -4,6 +4,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 
 
 @NgModule({
@@ -11,7 +14,10 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
     MatToolbarModule,
     MatTableModule,
     MatCardModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class AppMaterialModule { }
