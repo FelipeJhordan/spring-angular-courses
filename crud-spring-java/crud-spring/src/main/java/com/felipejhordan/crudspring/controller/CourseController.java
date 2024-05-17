4d03e052d83e4d249bf1b45a7ce5db297dcc1abb
felipejhordan <felipejordan.alves@gmail.com>
2024-05-17 11:44:24 -0300

    feat(back): save the course in bd

--- crud-spring-java/crud-spring/src/main/java/com/felipejhordan/crudspring/controller/CourseController.java
+++ crud-spring-java/crud-spring/src/main/java/com/felipejhordan/crudspring/controller/CourseController.java
@@ -3,8 +3,13 @@ package com.felipejhordan.crudspring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipejhordan.crudspring.model.Course;
@@ -22,5 +27,12 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+
     }
 }
--- crud-spring-java/crud-spring/src/main/java/com/felipejhordan/crudspring/model/Course.java
+++ crud-spring-java/crud-spring/src/main/java/com/felipejhordan/crudspring/model/Course.java
@@ -16,6 +16,7 @@ public class Course {
     @Id
     @GeneratedValue( strategy = GenerationType.AUTO)
     @JsonProperty("_id")
+    @Column
     private Long id;
 
     @Column(length = 200, nullable = false)
